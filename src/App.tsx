d9714eeaafc307c64b9c7dc93210691fcff59cc9
Andrii Holovchak <andrii.holovchak@yalantis.net>
2026-01-22 00:03:13 +0200

    use incorrect types

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import './App.css'
+import './App.css';
 
-
+const a: string = 3;
+console.log(a);
 
 function App() {
-  return (
-      <div className="App">
-      </div>
-  )
+  return <div className="App"></div>;
 }
 
 export default App;
